f759947ea86290829c7792a141862a34cee532d6
giobaltz <34406186+giobaltz@users.noreply.github.com>
2018-01-20 01:57:32 +0200

    Finding What I Supposed To Do

--- Askisi8.py
+++ Askisi8.py
@@ -2,3 +2,16 @@ import random
 
 num=random.sample(range(-30,30),30)
 print (num)
+
+for i in range(num):
+    for j in range(i+1,num):
+        y=-(num(i)+num(j))
+            if (y in num):
+                print (num(i),num(j),num(y))
+                num.pop(i)
+                num.pop(j)
+                num.pop(num.index(y))
+            else:
+                break
+
+print ("Αυτοί οι αριθμοί δεν μπορούν να βγάλουν αθροισμά 0", num)

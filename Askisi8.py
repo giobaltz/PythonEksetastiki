ceb6409dd2903886ada37584c9aebb639c68c14e
giobaltz <34406186+giobaltz@users.noreply.github.com>
2018-01-20 02:29:41 +0200

    Small improvements

--- Askisi8.py
+++ Askisi8.py
@@ -1,17 +1,16 @@
 import random
 
-num=random.sample(range(-30,30),30)
-print (num)
+num=random.sample(range(-30, 30), 30)
+print(num)
+for i in range(0, len(num)):
+    for j in range(i+1, len(num)):
+        y=-int(num(i))-int(num(j))
+        if (y in num):
+            print(int(num(i)), int(num(j)), int(num(y)))
+            num.pop(i)
+            num.pop(j)
+            num.pop(num.index(y))
+        else:
+            break
 
-for i in range(num):
-    for j in range(i+1,num):
-        y=-(num(i)+num(j))
-            if (y in num):
-                print (num(i),num(j),num(y))
-                num.pop(i)
-                num.pop(j)
-                num.pop(num.index(y))
-            else:
-                break
-
-print ("Αυτοί οι αριθμοί δεν μπορούν να βγάλουν αθροισμά 0", num)
+print("Autoi oi ariumoi den mporoun na dosoun athrisma 0", num)

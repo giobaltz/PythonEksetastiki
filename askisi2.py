062b28463ab20a7d4af37adc8804a4dcbe182ffa
giobaltz <34406186+giobaltz@users.noreply.github.com>
2018-02-10 01:51:37 +0200

    Whole idea ..but some small bugs

--- askisi2.py
+++ askisi2.py
@@ -6,10 +6,12 @@ day=date.today().day
 month=date.today().month
 year=date.today().year
 
-num = input("Give 10 KINO numbers (1,80) and between them use space ' ' \n").split(" ")
+num = input("Give 10 unique KINO numbers (1,80) and between them use space ' ' \n").split(" ")
 num_list=list(map(int, num))
+day_win=[]
 
 for x in range(1, day+1):
+    day_win.append(0)
     if x<10:
         y="0"+str(x)
     if month<10:
@@ -19,4 +21,19 @@ for x in range(1, day+1):
     data=json.loads(read_site.decode('utf-8'))
     last = len(data['draws']['draw'])
     for a in range(last):
-         print (data['draws']['draw'][a]['results'])
+        result = data['draws']['draw'][a]['results']
+        o=0
+        for i in range(10):
+            if (num_list[i] in result):
+                o+=1
+            if o==4:
+                day_win.append(x+1)
+                break
+
+max_win=max(day_win)
+win=[]
+for x in range(1, day+1):
+    if max_win==day_win[x]:
+        win.append(x)
+
+print ("Most of the successes in this month's KINO will be on the day(s)", win)

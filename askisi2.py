fbc85fb8d6c31cf57c11de8bd6d40e21bee4bbaf
giobaltz <34406186+giobaltz@users.noreply.github.com>
2018-02-10 18:03:16 +0200

    Beta Version

--- askisi2.py
+++ askisi2.py
@@ -8,15 +8,19 @@ year=date.today().year
 
 num = input("Give 10 unique KINO numbers (1,80) and between them use space ' ' \n").split(" ")
 num_list=list(map(int, num))
-day_win=[]
+day_win=[0]
 
 for x in range(1, day+1):
     day_win.append(0)
     if x<10:
-        y="0"+str(x)
+        d="0"+str(x)
+    else:
+        d=str(x)
     if month<10:
         str_month="0"+str(month)
-    site = urllib.request.Request("http://applications.opap.gr/DrawsRestServices/kino/drawDate/" + y + "-" + str_month + "-" + str(year) + ".json")
+    else:
+        str_month=str(month)
+    site = urllib.request.Request("http://applications.opap.gr/DrawsRestServices/kino/drawDate/" + d + "-" + str_month + "-" + str(year) + ".json")
     read_site = urllib.request.urlopen(site).read()
     data=json.loads(read_site.decode('utf-8'))
     last = len(data['draws']['draw'])
@@ -27,13 +31,16 @@ for x in range(1, day+1):
             if (num_list[i] in result):
                 o+=1
             if o==4:
-                day_win.append(x+1)
+                counter=day_win.pop()
+                counter+=1
+                day_win.append(counter)
                 break
 
-max_win=max(day_win)
-win=[]
-for x in range(1, day+1):
-    if max_win==day_win[x]:
-        win.append(x)
 
-print ("Most of the successes in this month's KINO will be on the day(s)", win)
+if max(day_win)==0:
+    print("Your not lucky :-(")
+else:
+    print("Most of the successes in this month's KINO will be on the day(s) ")
+    for i in range(1, day + 1):
+        if max(day_win) == day_win[i]:
+            print(i)
